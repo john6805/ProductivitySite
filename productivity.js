e74d34b2288ea796a36b5169c8a1a11eb2ffaa2a
Jacob Johnson <john6805@stthomas.edu>
2018-09-24 11:44:54 -0500

    navbar and basic add item function

--- productivity.js
+++ productivity.js
@@ -1,1 +1,13 @@
+function AddItem() 
+{
+    var item = document.getElementById("item");
+    var date = document.getElementById("date");
+    var category = document.getElementById("category");
+    var list = document.getElementById("todo-list");
 
+    var text = document.createTextNode(item.nodeValue + " " + date.nodeValue + " " + category.nodeValue);
+    var li = document.createElement("li");
+
+    li.appendChild(text);
+    list.appendChild(li);
+}
